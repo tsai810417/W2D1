93aed954cb6688186d092fb8bb1c469805a880b9
Cheng Tsai <tsai_cheng@yahoo.com>
2018-02-19 14:37:18 -0800

    Create Employee claass and add to Manager employees' list

---
+++ class_inheritance/employee.rb
@@ -0,0 +1,14 @@
+require_relative "manager"
+class Employee
+  attr_reader :name, :title, :salary, :boss
+
+  def initialize(name, title, salary, boss)
+    @name = name
+    @title = title
+    @salary = salary
+    @boss = boss
+    boss.add_employee(self) if boss
+  end
+
+
+end
