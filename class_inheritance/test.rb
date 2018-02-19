b54a473fe8a138ce98e92bce7ecbc70258f34821
Cheng Tsai <tsai_cheng@yahoo.com>
2018-02-19 14:38:16 -0800

    Create test file

---
+++ class_inheritance/test.rb
@@ -0,0 +1,12 @@
+load "employee.rb"
+load "manager.rb"
+ned = Manager.new("Ned", "Founder", 1000000, nil)
+darren = Manager.new("Darren", "TA Manager", 78000, ned)
+shawna = Employee.new("Shawna", "TA", 12000, darren)
+david = Employee.new("David", "TA", 10000, darren)
+
+p darren.employees
+
+# ned.bonus(5) # => 500_000
+# darren.bonus(4) # => 88_000
+# david.bonus(3) # => 30_000
